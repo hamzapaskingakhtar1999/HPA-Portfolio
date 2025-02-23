91d941fa63b7f43a5e5c961f975269916bdec22a
Hamza Pasking Akhtar <hamzapaskingakhtar@gmail.com>
2025-02-23 03:47:33 +0000

    grid responsiveness added

--- src/components/Grid/Grid.jsx
+++ src/components/Grid/Grid.jsx
@@ -1,18 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import style from "./Grid.module.css";
 
 import { FaShieldHeart } from "react-icons/fa6";
+import { MdLocationPin } from "react-icons/md";
+import { FaLaptopCode } from "react-icons/fa";
 
 /* Globe */
-import Globe from "react-globe.gl";
+import globe from "../../assets/Home/globe.png";
+import logo from "../../assets/logo.png";
 
 const Grid = () => {
+  const [currentTime, setCurrentTime] = useState(getCurrentTime());
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentTime(getCurrentTime());
+    }, 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+  function getCurrentTime() {
+    const date = new Date();
+    const options = {
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+      hour12: true,
+    };
+    return new Intl.DateTimeFormat("en-US", options).format(date).slice(0, 8);
+  }
+
   return (
     <div className={`${style.grid} compress`}>
       {/* Collaboration */}
       <div className={`${style.box1} ${style.box}`}>
-        <FaShieldHeart size={50} color="gray" />
+        <FaShieldHeart size={50} color="lightslategray" />
         <h3 className={style.box1SubHeading}>Collaboration and Satisfaction</h3>
         <h2 className={style.box1Heading}>
           I prioritize open communication and client satisfaction
@@ -28,13 +51,41 @@ const Grid = () => {
           </h1>
           <h1 className={style.box2SubHeading}>timezone communications</h1>
         </div>
+        <img className={style.box2Globe} src={globe}></img>
         <div className={style.box2Bottom}>
-
+          <MdLocationPin
+            size={50}
+            color="lightslategray"
+            style={{ marginBottom: "10px" }}
+          />
+          <h3 className={style.box2BottomSubHeading}>Remote</h3>
+          <h2 className={style.box2BottomHeading}>Ireland - {currentTime}</h2>
         </div>
       </div>
 
-      <div className={`${style.box} ${style.box3}`}></div>
-      <div className={`${style.box} ${style.box4}`}></div>
+      {/* Tech Enthusiast */}
+      <div className={`${style.box} ${style.box3}`}>
+        <div className={style.box3Bottom}>
+          <FaLaptopCode
+            size={50}
+            color="lightslategray"
+            style={{ marginBottom: "10px" }}
+          />
+          <h3 className={style.box3BottomSubHeading}>Tech Enthusiast</h3>
+          <h2 className={style.box3BottomHeading}>
+            Passionate about cutting-edge development technologies
+          </h2>
+        </div>
+      </div>
+      <div className={`${style.box} ${style.box4}`}>
+        <img src={logo} className={style.box4Logo} />
+        <h1 className={style.box4Heading}>
+          Let's work together on your next project
+        </h1>
+        <a className={style.button} href="mailto:hamzapaskingakhtar@gmail.com">
+          Send an email
+        </a>
+      </div>
       <div className={`${style.box} ${style.box5}`}></div>
     </div>
   );
